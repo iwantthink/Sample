9780aa046280e89014a7320d76f1640dba50e0d9
renbo <mychocolateisus@gmail.com>
2017-05-12 15:14:05 +0800

    add hmtl file

--- app/src/main/java/com/hypers/www/sample/activity/WebViewActivity.java
+++ app/src/main/java/com/hypers/www/sample/activity/WebViewActivity.java
@@ -1,7 +1,9 @@
 package com.hypers.www.sample.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -10,6 +12,11 @@ import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import com.hypers.www.sample.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -30,18 +37,54 @@ public class WebViewActivity extends AppCompatActivity {
         WebSettings settings = mWebView.getSettings();
         settings.setJavaScriptEnabled(true);
         mWebView.loadUrl("file:///android_asset/x.html");
+
         mWebView.addJavascriptInterface(this, "android");
+
+//        Object object = HMT.getInterface(WebViewActivity.this);
+//
+//        mWebView.addJavascriptInterface(object, "hmt");
+    }
+
+
+    @JavascriptInterface
+    public void show(String str, String json) {
+        Log.d("WebViewActivity", json);
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            Iterator<String> iterator = jsonObject.keys();
+            while (iterator.hasNext()) {
+                String key = iterator.next();
+                Log.d("WebViewActivity", key);
+                String value = (String) jsonObject.get(key);
+                Log.d("WebViewActivity", value);
+
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        Toast.makeText(this, " msg from js: " + str, Toast.LENGTH_SHORT).show();
     }
 
     @JavascriptInterface
-    public void show(String s) {
-        Toast.makeText(this, "hello this is from android", Toast.LENGTH_SHORT).show();
-        //111
-        //3222
+    public void show(String str) {
+        Toast.makeText(this, "param 1 msg = " + str, Toast.LENGTH_SHORT).show();
     }
 
-    @JavascriptInterface
-    public void show() {
-        Toast.makeText(this, "hello this is from android without param", Toast.LENGTH_SHORT).show();
+    public static class HMT {
+        public static Object getInterface(final Activity activity) {
+
+            return new Object() {
+
+                @JavascriptInterface
+                public void show(String str) {
+                    Toast.makeText(activity, "this is from .....HMT" + str, Toast.LENGTH_SHORT).show();
+                }
+            };
+
+        }
     }
 }
+
